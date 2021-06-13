979c6aac81cdf10df5eced06002690da36949b23
faskan <faisalkhanthayub@gmail.com>
2021-06-14 01:09:37 +0200

    cleanup

--- src/main/kotlin/com/faskan/todoksb/web/TodoResource.kt
+++ src/main/kotlin/com/faskan/todoksb/web/TodoResource.kt
@@ -2,10 +2,8 @@ package com.faskan.todoksb.web
 
 import com.faskan.todoksb.model.Todo
 import com.faskan.todoksb.repo.TodoRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 const val URL: String = "/api/todos"
 
@@ -15,9 +13,24 @@ class TodoResource(val todoRepository: TodoRepository) {
     fun getAllTodos(): List<Todo> {
         return todoRepository.findAll()
     }
+    @GetMapping("$URL/{id}")
+    fun getTodo(@PathVariable id: String): Optional<Todo> {
+        return todoRepository.findById(id)
+    }
 
     @PostMapping(URL)
     fun createTodo(@RequestBody todo: Todo): Todo {
         return todoRepository.save(todo)
+    }
+
+    @PutMapping("$URL/{id}")
+    fun updateTodo(@PathVariable id: String?, @RequestBody todo: Todo): Todo? {
+        val updatedTodo = Todo(id, todo.name, todo.description)
+        return todoRepository.save(updatedTodo)
+    }
+
+    @DeleteMapping("$URL/{id}")
+    fun deleteTodo(@PathVariable id: String?) {
+        todoRepository.delete(Todo(id, "", ""))
     }
 }
--- src/test/kotlin/com/faskan/todoksb/web/TodoResourceIT.kt
+++ src/test/kotlin/com/faskan/todoksb/web/TodoResourceIT.kt
@@ -1,7 +1,9 @@
 package com.faskan.todoksb.web
 
 import com.faskan.todoksb.model.Todo
+import com.faskan.todoksb.repo.TodoRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.skyscreamer.jsonassert.JSONAssert
 import org.skyscreamer.jsonassert.JSONCompareMode
@@ -19,18 +21,59 @@ import org.springframework.util.MultiValueMap
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoResourceIT(
     @Autowired val testRestTemplate: TestRestTemplate,
-    @LocalServerPort val port: Int = 0
+    @LocalServerPort val port: Int = 0,
+    @Autowired val todoRepository: TodoRepository
 ) {
 
-    @Test
-    fun `should return status ok`() {
-        val responseEntity = testRestTemplate.getForEntity(
-            uri(), String::class.java
-        )
-        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+    private fun uri() = "http://localhost:$port/api/todos"
+
+    @BeforeEach
+    fun init() {
+        todoRepository.deleteAll()
     }
 
-    private fun uri() = "http://localhost:$port/api/todos"
+    @Test
+    fun `should return all todos`() {
+        todoRepository.save(Todo(null, "Find", "Find the letter F"))
+        todoRepository.save(Todo(null, "Replace", "Replace id with K"))
+
+        val todosResponse = testRestTemplate.getForEntity(
+            uri(), String::class.java
+        )
+        assertThat(todosResponse.statusCode).isEqualTo(HttpStatus.OK)
+        JSONAssert.assertEquals(
+            """
+                [
+                    {
+                        "name" : "Find",
+                        "description" : "Find the letter F"
+                    },
+                    {
+                        "name" : "Replace",
+                        "description" : "Replace id with K"
+                    }
+                ]
+                """, todosResponse.body, JSONCompareMode.LENIENT
+        );
+    }
+
+    @Test
+    fun `should return Todo by id`() {
+        val todo = todoRepository.save(Todo(null, "Find", "Find the letter F"));
+        todoRepository.save(Todo(null, "Replace", "Replace it by K"));
+        var todosResponse = testRestTemplate.getForEntity(
+            uri() + "/{id}", String::class.java, todo.id
+        );
+        assertThat(todosResponse.statusCode).isEqualTo(HttpStatus.OK);
+        JSONAssert.assertEquals(
+            """
+                    {
+                        "name" : "Find",
+                        "description" : "Find the letter F"
+                    }
+                """, todosResponse.body, JSONCompareMode.LENIENT
+        );
+    }
 
     @Test
     fun `should save the todo and return all todos on get`() {
@@ -50,6 +93,40 @@ class TodoResourceIT(
                           }
                         ]""", todosResponse.body, JSONCompareMode.LENIENT
         )
+    }
+
+    @Test
+    fun `should update the todo`() {
+        val todo = todoRepository.save(Todo(null, "Dont Deploy", "Do not deploy to prod"))
+        testRestTemplate.put(uri() + "/{id}", request(), todo.id)
+        var todosResponse = testRestTemplate.getForEntity(
+            uri() + "/{id}", String::class.java, todo.id
+        )
+        assertThat(todosResponse.statusCode).isEqualTo(HttpStatus.OK)
+        JSONAssert.assertEquals(
+            """
+                    {
+                        "name" : "Deploy",
+                        "description" : "Deploy to prod"
+                    }
+                """, todosResponse.body, JSONCompareMode.LENIENT
+        );
+    }
+
+    @Test
+    fun `should delete todo`() {
+        val todo = todoRepository.save(Todo(null, "Dont Deploy", "Do not deploy to prod"))
+        testRestTemplate.delete(uri() + "/{id}", todo.id)
+        var todosResponse = testRestTemplate.getForEntity(
+            uri() + "/", String::class.java, todo.id
+        )
+        assertThat(todosResponse.statusCode).isEqualTo(HttpStatus.OK)
+        JSONAssert.assertEquals(
+            """
+                [
+                ]
+                """, todosResponse.body, JSONCompareMode.STRICT
+        );
     }
 
     private fun request(): HttpEntity<String> {
